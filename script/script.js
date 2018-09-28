70e8720cc9ce5955a47738fca4d8c51ee237716d
alessouza <alessandrasilvasouza@live.com>
2018-09-28 14:05:17 -0300

    add drag drop

--- script/script.js
+++ script/script.js
@@ -9,28 +9,29 @@ inputText.addEventListener("keypress", function (event) {
   }
 })
 
-
 button.addEventListener("click", add);
 
-function add() {
+let dragging;
 
+function add() {
   if (inputText.value === null || inputText.value === undefined || inputText.length === 0 || !inputText.value.trim()) {
     inputText.focus();
     return false;
   }
 
   const newTask = document.createElement("div");
-  newTask.className = "to-do-list__new-task";
-
   const task = document.createElement("p");
+  const remove = document.createElement("div");
+
+  newTask.className = "to-do-list__new-task";
+  task.className = "to-do-list__new-task_name";
+  remove.className = "to-do-list__new-task_remove";
+
+  task.innerHTML = inputText.value;
+  remove.innerHTML = `<button class="to-do-list__new-task_remove-button">x</button>`
+
   task.style.width = "80%";
   task.style.wordWrap = "break-word";
-  task.className = "to-do-list__new-task_name";
-  task.innerHTML = inputText.value;
-
-  const remove = document.createElement("div");
-  remove.className = "to-do-list__new-task_remove";
-  remove.innerHTML = `<button class="to-do-list__new-task_remove-button">x</button>`
 
   newTask.appendChild(task);
   newTask.appendChild(remove);
@@ -53,23 +54,39 @@ function add() {
     newTask.remove();
   })
 
+  newTask.setAttribute("draggable", true);
 
+  newTask.addEventListener("dragstart", dragStart);
+  newTask.addEventListener('dragover', dragOver);
+  newTask.addEventListener("dragend", dragEnd);
+
+  function dragStart(event) {
+    dragging = event.target.closest(".to-do-list__new-task");
+  }
+
+  function dragOver(event) {
+    const location = event.target.closest(".to-do-list__new-task");
+    console.log(dragging);
+    this.parentNode.insertBefore(dragging, location);
+  }
+  function dragEnd() {
+    dragging = null;
+  }
 };
 
 const buttons = document.createElement("div");
-buttons.className = "buttons";
-
 const buttonCheck = document.createElement("button");
-buttonCheck.className = "to-do-list_buttons";
-buttonCheck.innerHTML = "check all";
-
 const buttonClear = document.createElement("button");
+
+buttons.className = "buttons";
+buttonCheck.className = "to-do-list_buttons";
 buttonClear.className = "to-do-list_buttons";
+
+buttonCheck.innerHTML = "check all";
 buttonClear.innerHTML = "clear all";
 
 buttons.appendChild(buttonCheck);
 buttons.appendChild(buttonClear);
-
 container.insertBefore(buttons, container.childNodes[container.length]);
 
 buttonCheck.addEventListener("click", function () {
@@ -103,5 +120,4 @@ buttonClear.addEventListener("click", function () {
     toDoListNewTask[i].remove();
   }
 })
-
 
